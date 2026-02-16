00d9a0912c1047f3ca51d462036fb981149e6333
RyanNeuville <neuvilleryan123@gmail.com>
2026-02-16 02:11:21 +0100

    feat: add Responsable class extends with Utilisateurs class.

---
+++ services/src/main/java/com/codexmaker/services/rest/model/entity/Responsable.java
@@ -0,0 +1,23 @@
+package com.codexmaker.services.rest.model.entity;
+
+import jdk.jshell.execution.Util;
+
+import java.util.List;
+
+public class Responsable extends Utilisateur {
+
+    /** Methodes metier */
+    public List<DemandeConge> consulterDemandesATraiter(){
+        return null;
+    }
+
+    public void validerDemande(String demandeId, String commentaire){
+    }
+
+    public void refuserDemande(String demandeId, String commentaire){
+    }
+
+    public void ajouterCommentaireValidation(String demandeId, String commentaire){
+    }
+
+}
