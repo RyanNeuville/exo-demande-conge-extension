c4cb107388858f2366dc48b5af3b570a97af0b86
RyanNeuville <neuvilleryan123@gmail.com>
2026-02-16 02:28:21 +0100

    feat: add Administrateur class extends with Utilisateurs class.

---
+++ services/src/main/java/com/codexmaker/services/rest/model/entity/Administrateur.java
@@ -0,0 +1,15 @@
+package com.codexmaker.services.rest.model.entity;
+
+public class Administrateur extends Utilisateur{
+
+    /** Methodes metier */
+    public List<DemandeConge> consulterToutesLesDemandes(){
+        return null;
+    }
+
+    public void supprimerDemande(String demandeId){
+    }
+
+    public void gererTypesConges(){
+    }
+}
