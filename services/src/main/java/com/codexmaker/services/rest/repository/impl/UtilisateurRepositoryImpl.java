bb276707ee13b339fa1f694c0cae120384550d64
RyanNeuville <neuvilleryan123@gmail.com>
2026-02-20 18:43:03 +0100

    refactor: remove UtilisateurRepository interface implementation and related imports from UtilisateurRepositoryImpl.

--- services/src/main/java/com/codexmaker/services/rest/repository/impl/UtilisateurRepositoryImpl.java
+++ services/src/main/java/com/codexmaker/services/rest/repository/impl/UtilisateurRepositoryImpl.java
@@ -1,10 +1,5 @@
 package com.codexmaker.services.rest.repository.impl;
 
-import com.codexmaker.services.rest.model.entity.Utilisateur;
-import com.codexmaker.services.rest.repository.UtilisateurRepository;
+public class UtilisateurRepositoryImpl {
 
-public class UtilisateurRepositoryImpl implements UtilisateurRepository {
-
-
-    
 }
