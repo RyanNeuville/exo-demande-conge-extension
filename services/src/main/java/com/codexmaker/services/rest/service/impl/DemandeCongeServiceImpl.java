6788afa9e69067d96aee1c104f847c5055e981df
RyanNeuville <neuvilleryan123@gmail.com>
2026-02-24 18:07:33 +0100

    feat: clean backend architecture. move rest controller and create service and implementation

--- services/src/main/java/com/codexmaker/services/rest/service/impl/DemandeCongeServiceImpl.java
+++ services/src/main/java/com/codexmaker/services/rest/service/impl/DemandeCongeServiceImpl.java
@@ -1,6 +1,60 @@
 package com.codexmaker.services.rest.service.impl;
+
+import com.codexmaker.services.rest.model.entity.DemandeConge;
+import com.codexmaker.services.rest.repository.DemandeCongeRepository;
+import com.codexmaker.services.rest.repository.impl.DemandeCongeRepositoryImpl;
+import com.codexmaker.services.rest.repository.UtilisateurRepository;
+import com.codexmaker.services.rest.repository.impl.UtilisateurRepositoryImpl;
 import com.codexmaker.services.rest.service.DemandeCongeService;
 
+import java.util.List;
+
+/**
+ * Implémentation du service métier pour les demandes de congés.
+ */
 public class DemandeCongeServiceImpl implements DemandeCongeService {
-    
+
+    private final DemandeCongeRepository demandeCongeRepository;
+    private final UtilisateurRepository utilisateurRepository;
+
+    public DemandeCongeServiceImpl() {
+        this.demandeCongeRepository = new DemandeCongeRepositoryImpl();
+        this.utilisateurRepository = new UtilisateurRepositoryImpl();
+    }
+
+    @Override
+    public DemandeConge soumettreDemande(DemandeConge demande, String userId) {
+        // TODO: Vérifier le solde, vérifier les chevauchements, persister la demande
+        return null;
+    }
+
+    @Override
+    public List<DemandeConge> getDemandesParUtilisateur(String userId) {
+        return demandeCongeRepository.findByUserId(userId);
+    }
+
+    @Override
+    public List<DemandeConge> getToutesLesDemandes() {
+        return demandeCongeRepository.findAll();
+    }
+
+    @Override
+    public List<DemandeConge> getDemandesATraiter(String valideurId) {
+        return demandeCongeRepository.findPendingForValidator(valideurId);
+    }
+
+    @Override
+    public void validerDemande(String demandeId, String commentaire) {
+        // TODO: Mettre à jour statut + historiser + débiter le solde
+    }
+
+    @Override
+    public void refuserDemande(String demandeId, String commentaire) {
+        // TODO: Mettre à jour statut + historiser
+    }
+
+    @Override
+    public void annulerDemande(String demandeId) {
+        // TODO: Mettre à jour statut + historiser
+    }
 }
